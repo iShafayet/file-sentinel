5b3eeb6e624d0b8b9301437e178019a7aff661e6
Sayem Shafayet <ishafayet@gmail.com>
2025-01-26 17:57:39 +0600

    Refactor

--- src/service/integrity-service.ts
+++ src/service/integrity-service.ts
@@ -7,9 +7,16 @@ import { FileMetaData } from "../model/file-meta-data.js";
 import fs from "fs";
 import { cryptoService } from "./crypto-service.js";
 
+export interface ListMap {
+  skipped: string[];
+  passed: string[];
+  failed: string[];
+  error: string[];
+}
+
 class IntegrityService {
 
-  private async verifyFile(filePath: string, rootDir: string, metaDataRootDir: string, verificationMode: VerificationMode, counter: { verified: number; outdated: number; failed: number; }): Promise<void> {
+  private async verifyFile(filePath: string, rootDir: string, metaDataRootDir: string, verificationMode: VerificationMode, listMap: ListMap): Promise<void> {
     const relativeFilePath = filePath.replace(rootDir, "");
     logger.log(`(tagging-service)> Potentially updating file: ${relativeFilePath}`);
     const metaDataPath = getMetaFilePath(filePath, rootDir, metaDataRootDir);
@@ -18,26 +25,26 @@ class IntegrityService {
     const { error } = fileMetaDataSchema.validate(existingMeta);
     if (error) {
       logger.log(`(integrity-service)> ERROR: File ${relativeFilePath} has invalid meta data. Verification failed.`);
-      counter.failed++;
+      listMap.error.push(relativeFilePath);
       return;
     }
 
     const fileStat = fs.statSync(filePath);
 
     if (existingMeta.file.modifiedAt !== fileStat.mtime.getTime()) {
-      logger.log(`(integrity-service)> File ${relativeFilePath} has been modified (modifiedAt from meta: ${existingMeta.file.modifiedAt}, modifiedAt from file: ${fileStat.mtime.getTime()}). It will be re-tagged.`);
-      counter.outdated++;
+      logger.log(`(integrity-service)> File ${relativeFilePath} has been modified (modifiedAt from meta: ${existingMeta.file.modifiedAt}, modifiedAt from file: ${fileStat.mtime.getTime()}). It will be skipped.`);
+      listMap.skipped.push(relativeFilePath);
       return;
     }
 
     if (verificationMode === "size") {
       if (existingMeta.file.size !== fileStat.size) {
-        logger.log(`(integrity-service)> File ${relativeFilePath} has been modified (size from meta: ${existingMeta.file.size}, size from file: ${fileStat.size}). It will be re-tagged.`);
-        counter.outdated++;
+        logger.log(`(integrity-service)> File ${relativeFilePath} has been corrupted (size from meta: ${existingMeta.file.size}, size from file: ${fileStat.size}) (changed without updating modifiedAt). Failed verification.`);
+        listMap.failed.push(relativeFilePath);
         return;
       } else {
         logger.log(`(integrity-service)> File ${relativeFilePath} is unchanged (size check passed)`);
-        counter.verified++;
+        listMap.passed.push(relativeFilePath);
         return;
       }
     }
@@ -45,12 +52,12 @@ class IntegrityService {
     if (verificationMode === "size-and-hash") {
       const hash = await cryptoService.generateSha256HashFromFile(filePath, fileStat.size);
       if (existingMeta.hash.sha256 !== hash) {
-        logger.log(`(integrity-service)> File ${relativeFilePath} has been modified (hash from meta: ${existingMeta.hash.sha256}, hash from file: ${hash}). It will be re-tagged.`);
-        counter.outdated++;
+        logger.log(`(integrity-service)> File ${relativeFilePath} has been corrupted (hash from meta: ${existingMeta.hash.sha256}, hash from file: ${hash}) (changed without updating modifiedAt). Failed verification.`);
+        listMap.failed.push(relativeFilePath);
         return;
       } else {
         logger.log(`(integrity-service)> File ${relativeFilePath} is unchanged (hash check passed)`);
-        counter.verified++;
+        listMap.passed.push(relativeFilePath);
         return;
       }
     }
@@ -58,8 +65,15 @@ class IntegrityService {
     throw new Error("Code should not reach here");
   }
 
-  public async verifyIntegrity(config: Config): Promise<void> {
+  public async verifyIntegrity(config: Config): Promise<ListMap> {
     logger.log("(integrity-service)> Verifying integrity");
+
+    const listMap: ListMap = {
+      passed: [],
+      failed: [],
+      error: [],
+      skipped: [],
+    };
 
     const untaggedFileList: string[] = [];
     const previouslyTaggedFileList: string[] = [];
@@ -73,24 +87,22 @@ class IntegrityService {
       logger.log(`(integrity-service)> Found ${previouslyTaggedFileList.length} previously tagged files. These will be verified.`);
     } else {
       logger.log(`(integrity-service)> No previously tagged files found. Nothing to verify.`);
-      return;
+      return listMap;
     }
 
-    const counter = {
-      verified: 0,
-      outdated: 0,
-      failed: 0,
-    };
     for (const filePath of previouslyTaggedFileList) {
-      await this.verifyFile(filePath, config.target.dir, config.target.metaDataDir || config.target.dir, config.verification.mode, counter);
+      await this.verifyFile(filePath, config.target.dir, config.target.metaDataDir || config.target.dir, config.verification.mode, listMap);
     }
 
     logger.log(
       "(integrity-service)> Verification complete.\n" +
-      `    • ${counter.verified} files verified\n` +
-      `    • ${counter.outdated} files outdated\n` +
-      `    • ${counter.failed} files failed`
+      `    • ${listMap.skipped.length} files skipped\n` +
+      `    • ${listMap.passed.length} files passed\n` +
+      `    • ${listMap.failed.length} files failed\n` +
+      `    • ${listMap.error.length} files generated errors`
     );
+
+    return listMap;
   }
 }
 
