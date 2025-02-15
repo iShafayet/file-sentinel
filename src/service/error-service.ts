862db35314dc0440d9f8f6b5d0dc41fa27843d56
Sayem Shafayet <ishafayet@gmail.com>
2025-02-15 21:54:49 +0600

    Add option panic on error

---
+++ src/service/error-service.ts
@@ -0,0 +1,34 @@
+import { logger } from "../lib/logger.js";
+import { Config } from "../model/config.js";
+
+class ErrorService {
+
+  private config: Config | null = null;
+
+  setConfig(config: Config): void {
+    this.config = config;
+  }
+
+  handleErrorDuringIteration(error: Error | unknown): void {
+    if (!this.config) {
+      throw new Error("Fatal error: Config is not set");
+    }
+
+    let actualError: Error;
+    if (!(error instanceof Error)) {
+      actualError = new Error(String(error));
+    } else {
+      actualError = error;
+    }
+
+    logger.error(actualError);
+
+    if (this.config.panicOnError) {
+      logger.log("(error-service)> Panicking due to error");
+      process.exit(1);
+    }
+  }
+}
+
+export const errorService = new ErrorService();
+
