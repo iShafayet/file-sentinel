1136e4388f14482632c72d7f84caa6f5b54fb229
Sayem Shafayet <ishafayet@gmail.com>
2025-02-16 23:04:15 +0600

    Copy and verify large file with progress

--- src/service/file-service.ts
+++ src/service/file-service.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
-
+import constants from "../constant/common-constants.js";
 class FileService {
 
   verifyDirectoryExists(dir: string): boolean {
@@ -8,6 +8,43 @@ class FileService {
 
   verifyFileExists(file: string): boolean {
     return fs.existsSync(file) && fs.statSync(file).isFile();
+  }
+
+  async copyLargeFile(source: string, destination: string, updateProgressFn: (bytesRead: number) => void): Promise<void> {
+    return await new Promise<void>((resolve, reject) => {
+      try {
+        const readStream = fs.createReadStream(source);
+        const writeStream = fs.createWriteStream(destination);
+        let totalBytesRead = 0;
+        let lastProgressLogTime = 0;
+        readStream.on('data', (chunk: Buffer) => {
+          writeStream.write(chunk);
+          totalBytesRead += chunk.length;
+          if (Date.now() - lastProgressLogTime >= constants.PROGRESS_LOG_INTERVAL_MS) {
+            lastProgressLogTime = Date.now();
+            updateProgressFn(totalBytesRead);
+          }
+        });
+        readStream.on('end', () => {
+          writeStream.end();
+          updateProgressFn(totalBytesRead);
+          resolve();
+        });
+        readStream.on('error', (err: Error) => {
+          console.error('Error copying file:', err);
+          reject(err);
+        });
+        writeStream.on('error', (err: Error) => {
+          console.error('Error copying file:', err);
+          reject(err);
+        });
+        writeStream.on('close', () => {
+          resolve();
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
   }
 }
 
