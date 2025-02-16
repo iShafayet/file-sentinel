82cf0ebb5e339149272ad5c901ff02cff82e1da5
Sayem Shafayet <ishafayet@gmail.com>
2025-02-16 20:08:01 +0600

    Update test tooling

---
+++ scripts/post-build.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+import { dirname, join, normalize } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const testEnv = join(__dirname, "../test/.env.test");
+const distTestEnv = join(__dirname, "../dist/test/.env.test");
+
+if (fs.existsSync(testEnv)) {
+  fs.copyFileSync(testEnv, distTestEnv);
+}
